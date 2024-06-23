cb4c521f9f65a475d243c0f57d0620bcbbd291ba
k0kishima <56298669+k0kishima@users.noreply.github.com>
2024-06-23 16:51:15 +0900

    Add a role for sagemaker

--- ml/output.tf
+++ ml/output.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "The name of the S3 bucket for SageMaker"
   value       = aws_s3_bucket.sagemaker_bucket.bucket
 }
+
+output "role_arn" {
+  description = "The ARN of the SageMaker execution role"
+  value       = aws_iam_role.sagemaker_execution_role.arn
+}
--- ml/variables.tf
+++ ml/variables.tf
@@ -2,3 +2,9 @@ variable "bucket_name" {
   description = "The name of the S3 bucket to use for SageMaker"
   type        = string
 }
+
+variable "role_name" {
+  description = "The name of the IAM role for SageMaker"
+  type        = string
+  default     = "sagemaker-execution-role"
+}
